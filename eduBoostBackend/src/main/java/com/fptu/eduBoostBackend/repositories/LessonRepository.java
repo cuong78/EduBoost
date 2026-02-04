739181c447467f9f48131f9a4e47820e7cd5afc0
anhnkse24 <kyanhngo521@gmail.com>
2026-02-04 22:10:02 +0700

    add data and repository(Chapter, Lesson, LessonResource)

---
+++ eduBoostBackend/src/main/java/com/fptu/eduBoostBackend/repositories/LessonRepository.java
@@ -0,0 +1,14 @@
+package com.fptu.eduBoostBackend.repositories;
+
+import com.fptu.eduBoostBackend.entities.Chapter;
+import com.fptu.eduBoostBackend.entities.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+    List<Lesson> findByChapter(Chapter chapter);
+    List<Lesson> findByChapterOrderByLessonNumberAsc(Chapter chapter);
+}
